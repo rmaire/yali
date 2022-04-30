872e6a44ee28d883119622d9265e7caee7f9abdb
Roman Maire <roman.maire@zuara.ch>
2022-04-30 09:54:31 +0200

    Merge

--- src/test/java/ch/uprisesoft/yali/runtime/interpreter/InterpreterTest.java
+++ src/test/java/ch/uprisesoft/yali/runtime/interpreter/InterpreterTest.java
@@ -33,7 +33,6 @@ import org.junit.jupiter.api.Disabled;
  *
  * @author uprisesoft@gmail.com
  */
-
 //@Disabled
 public class InterpreterTest {
 
@@ -77,14 +76,14 @@ public class InterpreterTest {
         ObjectMother om = new ObjectMother(oo, ig);
         it = om.getInterpreter();
     }
-    
+
     @Test
     public void testExp1() {
         StringBuilder sb = new StringBuilder();
         sb.append("fd 60 rt 120 fd 60 rt 120 fd 60 rt 120").append("\n");
         Node res = it.run(it.read(sb.toString()));
     }
-    
+
     @Test
     public void testExp2() {
         StringBuilder sb = new StringBuilder();
@@ -92,7 +91,7 @@ public class InterpreterTest {
         sb.append("print :first_programmer").append("\n");
         Node res = it.run(it.read(sb.toString()));
     }
-    
+
     @Test
     public void testExp3() {
         StringBuilder sb = new StringBuilder();
@@ -100,7 +99,7 @@ public class InterpreterTest {
         sb.append("repeat 10 [fd 3 rt :angle make \"angle :angle + 7]").append("\n");
         Node res = it.run(it.read(sb.toString()));
     }
-    
+
     @Test
     public void testExp4() {
         StringBuilder sb = new StringBuilder();
@@ -139,7 +138,7 @@ public class InterpreterTest {
         assertThat(nte.getNode().getCol(), is(0));
 
     }
-    
+
     @Test
     public void testRecursion() {
         String input = "to recurse :i\n"
@@ -150,34 +149,14 @@ public class InterpreterTest {
                 + "recurse 8000\n";
 
         Node res = it.run(it.read(input));
-        
-        assertThat(outputs.size(), is(8001));
-        
-        for(int i = 8000; i >= 0; i--) {
-            assertThat(outputs.get(8000-i), is(i + "\n"));
-        }        
-    }
-    
-    @Disabled
-    @Test
-    public void testRecursion2() {
-        String input = "to square\n"
-                + "repeat 4 [fd 100 rt 90]\n"
-                + "end\n"
-                + "\n"
-                + "to draw\n"
-                + "square\n"
-                + "fd 5\n"
-                + "rt 7\n"
-                + "print turtlepos\n"
-                + "draw\n"
-                + "end\n"
-                + "\n"
-                + "draw\n";
 
-        Node res = it.run(it.read(input));       
+        assertThat(outputs.size(), is(8001));
+
+        for (int i = 8000; i >= 0; i--) {
+            assertThat(outputs.get(8000 - i), is(i + "\n"));
+        }
     }
-    
+
     @Test
     public void testNestedRunList() {
         String input = "to testit :i\n"
@@ -187,9 +166,30 @@ public class InterpreterTest {
                 + "testit 6\n";
 
         Node res = it.run(it.read(input));
-        
+
         assertThat(outputs.size(), is(2));
         assertThat(outputs.get(0), is("first\n"));
         assertThat(outputs.get(1), is("yes\n"));
+    }
+
+    @Test
+    public void testReset() {
+        String badInput = "Blabla\n";
+
+        try {
+            Node res = it.run(it.read(badInput));
+        } catch (NodeTypeException nte) {
+            assertThat(nte.getExpected().get(0), is(NodeType.PROCCALL));
+            assertThat(nte.getReceived(), is(NodeType.SYMBOL));
+        }
+
+        String goodInput = "print \"Blabla\n";
+
+        it.reset();
+        Node res = it.run(it.read(goodInput));
+        
+        assertThat(outputs.size(), is(1));
+        assertThat(outputs.get(0), is("Blabla\n"));
+
     }
 }
